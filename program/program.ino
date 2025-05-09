b1e9b88c2bcfb2f071de3e1f24b58ae956300994
KavindaKiridana <kavindakiridena@gmail.com>
2025-05-09 19:35:51 +0530

    Update program.ino

--- program/program.ino
+++ program/program.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
-// Define LED pins individually
-const int led1 = 2;
-const int led2 = 3;
-const int led3 = 4;
-const int led4 = 5;
+// Correct GPIO pins for ESP8266 (NodeMCU)
+const int led1 = D1;  // GPIO5
+const int led2 = D2;  // GPIO4
+const int led3 = D5;  // GPIO14
+const int led4 = D6;  // GPIO12
 
 void setup() {
   // Initialize each LED pin as output
@@ -12,34 +12,29 @@ void setup() {
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+  
+  // Turn all LEDs off initially
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
 }
 
 void loop() {
-  // Turn on only first LED
+  // Sequence with delays between each LED
   digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-    
-  // Turn on only second LED
+  delay(500);
   digitalWrite(led1, LOW);
+  
   digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  
+  digitalWrite(led3, HIGH);
+  delay(500);
   digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-
-  // Turn on only third LED
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, LOW);
-
-  // Turn on only fourth LED
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, HIGH);
+  
+  digitalWrite(led4, HIGH);
+  delay(500);
   digitalWrite(led4, LOW);
 }
-
-digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, HIGH);
